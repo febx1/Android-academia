97e15bc5c87689275b1cb7c9ebc63a15fec776f8
unknown <georgefebin97@gmail.com>
2022-03-26 05:30:24 +0530

    pg 2 complete

--- pgtwo/app/src/main/java/com/example/pgtwo/MainActivity.java
+++ pgtwo/app/src/main/java/com/example/pgtwo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pgtwo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE)
+        {
+            Toast toast =Toast.makeText(this,"this is landscape view",Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else if (newConfig.orientation==Configuration.ORIENTATION_PORTRAIT)
+        {
+            Toast toast = Toast.makeText(this, "this is portrait view", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
     }
 }
